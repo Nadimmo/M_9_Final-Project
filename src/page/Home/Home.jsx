d2288e796ab900def60f671493d87dac5c5a75d8
nadimmostafa <nadimmostafa334@gmail.com>
2024-06-06 14:50:03 +0600

    add category , cover and popular item

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,27 +1,27 @@
 // import Carts from "../Carts/Carts";
-// import Category from "../Category/Category";
+import Category from "../Category/Category";
 // import FeaturedMenu from "../FeaturedMenu/FeaturedMenu";
-// import PopularItem from "../PopularItem/PopularItem";
+import PopularItem from "../PopularItem/PopularItem";
 // import Testimonials from "../Testimonials/Testimonials";
 import Banner from "./Banner/Banner";
-// import Boss from "./Boss/Boss";
+import Boss from "./Boss/Boss";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      {/* <div className="mx-12">
+       <div className="mx-12">
         <Category></Category>
         <Boss></Boss>
         <PopularItem></PopularItem>
       </div>
-      <div className="mx-12">
+      {/* <div className="mx-12">
         <Carts></Carts>
       </div>
       <FeaturedMenu></FeaturedMenu>
       <div className="mx-12">
         <Testimonials></Testimonials>
-      </div> */}
+      </div>  */}
       <br />
       <br />
     </div>
